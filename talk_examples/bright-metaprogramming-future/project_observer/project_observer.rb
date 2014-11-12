d7361c2dd0592c81370a0c5fb60e80b0187b6085
Betsy Haibel <betsy.haibel@gmail.com>
2014-11-11 22:14:38 -0500

    alter project_observer.rb to use #send

--- talk_examples/bright-metaprogramming-future/project_observer/project_observer.rb
+++ talk_examples/bright-metaprogramming-future/project_observer/project_observer.rb
@@ -1,14 +1,9 @@
 class ProjectObserver < ActiveRecord::Observer
   def after_update(project)
-    case
-    when project.milestone_changed?
-      Mailer.send_later :milestone_changed, project.owner, project
-    when project.name_changed?
-      Mailer.send_later :name_changed, project.owner, project
-    when project.description_changed?
-      Mailer.send_later :description_changed, project.owner, project
-    when project.target_build_day_changed?
-      Mailer.send_later :target_build_day_changed, project.owner, project
+    [:milestone, :name, :description, :build_day].each do |attribute|
+      if project.send "#{attribute}_changed?"
+        Mailer.send_later "#{attribute}_changed", project.owner, project
+      end
     end
   end
 end
