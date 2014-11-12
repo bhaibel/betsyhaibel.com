3dd396e3efd512bfac818a8226a5f9a9269ae574
Betsy Haibel <betsy.haibel@gmail.com>
2014-11-12 00:02:48 -0500

    make class method on Formatter delegate to new instance

--- talk_examples/bright-metaprogramming-future/formatter/formatter.rb
+++ talk_examples/bright-metaprogramming-future/formatter/formatter.rb
@@ -1,9 +1,20 @@
 class Formatter
   def self.format_results(garment, user_type, results)
-    send("format_#{garment}_results_for_#{user_type}", results)
+    new(garment, user_type, results).format
   end
 
-  def self.format_sweater_results_for_admin(results)
+  attr_reader :garment, :user_type, :results
+  def initialize(garment, user_type, results)
+    @garment = garment
+    @user_type = user_type
+    @results = results
+  end
+
+  def format
+    send("format_#{garment}_results_for_#{user_type}")
+  end
+
+  def format_sweater_results_for_admin
     CSV.generate do |csv|
       results.each do |result|
         arr = [:id, :name, :yarn_name, :yardage, :sleeve_length, :currently_promoting?].map do |attribute|
@@ -14,13 +25,13 @@ class Formatter
     end
   end
 
-  def self.format_sweater_results_for_user(results)
+  def format_sweater_results_for_user
     results.map do |x|
       [:name, :yarn_name, :yardage, :sleeve_length].map { |attribute| result.send(attribute) } 
     end.to_json
   end
 
-  def self.format_hat_results_for_user(results)
+  def format_hat_results_for_user
     results.map do |x|
       [:name, :yarn_name, :earflaps?].map { |attribute| result.send(attribute) } 
     end.to_json
