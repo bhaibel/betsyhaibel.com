227ceb99ee593a89c4b31e94d679e0a64e7a0940
Betsy Haibel <betsy.haibel@gmail.com>
2014-11-12 00:34:35 -0500

    move to polymorphic approach for formatters

--- talk_examples/bright-metaprogramming-future/formatter/formatter.rb
+++ talk_examples/bright-metaprogramming-future/formatter/formatter.rb
@@ -1,26 +1,27 @@
 class Formatter
   def self.format_results(user_type, results)
-    new(user_type, results).format
+    const_get("#{user_type}Formatter", false).new(results).format
   end
 
-  attr_reader :user_type, :results
-  def initialize(user_type, results)
-    @user_type = user_type
-    @results = results
+  class AbstractFormatter
+    attr_reader :results
+    def initialize(results)
+      @results = results
+    end
   end
 
-  def format
-    send("format_for_#{user_type}")
+  class UserFormatter < AbstractFormatter
+    def format
+      results.map(&:to_formatter_data).to_json
+    end
   end
 
-  def format_for_user
-    results.map(&:to_formatter_data).to_json
-  end
-
-  def format_for_admin
-    CSV.generate do |csv|
-      results.each do |result|
-        csv << result.to_formatter_data(admin: true)
+  class AdminFormatter < AbstractFormatter
+    def format
+      CSV.generate do |csv|
+        results.each do |result|
+          csv << result.to_formatter_data(admin: true)
+        end
       end
     end
   end
