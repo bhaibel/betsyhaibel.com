9b24f83eecb8b0fedeebd97e77fc63062885731e
Betsy Haibel <betsy.haibel@gmail.com>
2014-11-13 01:09:24 -0500

    add super to fake AR

--- talk_examples/bright-metaprogramming-future/method_missing/active_record_base.rb
+++ talk_examples/bright-metaprogramming-future/method_missing/active_record_base.rb
@@ -2,6 +2,8 @@ class ActiveRecord::Base
   def method_missing(name, *args, &block)
     if name.ends_with('_changed?')
       attribute_changed?(name.gsub(/_changed?/, ''))
+    else
+      super
     end
   end
 end
