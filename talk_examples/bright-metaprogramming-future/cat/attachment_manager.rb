28f7bbc17cf435f011a3815e640a6c919337c8b4
Betsy Haibel <betsy.haibel@gmail.com>
2014-11-13 00:49:00 -0500

    attachment manager with anonymous module inclusion

--- talk_examples/bright-metaprogramming-future/cat/attachment_manager.rb
+++ talk_examples/bright-metaprogramming-future/cat/attachment_manager.rb
@@ -1,11 +1,14 @@
 module AttachmentManager
   def define_attachment(name)
-    attr_accessor name
+    instance_methods = Module.new do
+      attr_accessor name
 
-    %w(url name path).each do |method|
-      define_method "#{name}_#{method}" do
-        send(name).send(method)
+      %w(url name path).each do |method|
+        define_method "#{name}_#{method}" do
+          send(name).send(method)
+        end
       end
     end
+    include instance_methods
   end
 end
